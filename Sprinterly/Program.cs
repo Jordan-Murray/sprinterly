1bad3c444602d5df5ae5243674df2736ddac26e6
Jordan Murray <jlmurray100@gmail.com>
2023-11-16 22:21:36 +0000

    Changed swagger to be in prod too for OpenAI plugin usage

--- Sprinterly/Program.cs
+++ Sprinterly/Program.cs
@@ -66,11 +66,11 @@ var app = builder.Build();
 app.MapHealthChecks("/health");
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
